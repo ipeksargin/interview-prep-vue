e4223ff11844cd269510390ab7351686f1f4a717
ipek sargin <ipek@torkyazilim.com>
2021-04-20 20:28:02 +0300

    App component updated

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <v-app id="inspire">
     <v-main>
-        <SearchBar />
+      <SearchBar />
+      <v-container>
+        <MovieItem />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import SearchBar from "./components/SearchBar.vue";
-
+import MovieItem from "./components/MovieItem.vue";
 
 export default {
   data: () => ({}),
   components: {
-    SearchBar
+    SearchBar,
+    MovieItem,
   },
 };
 </script>
