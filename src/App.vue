8f2e4c96344c9320e14877ee39c55c5247e205ec
ipek sargin <ipek@torkyazilim.com>
2021-04-22 02:04:57 +0300

    App component updated

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,65 @@
 <template>
   <v-app id="inspire">
     <v-main>
-      <SearchBar />
+      <SearchBar
+        @getClickedValue="getAndPassClickedValue"
+        @getInputValue="searchByInputValue"
+        :isListOpen="isListOpen"
+        :movieList="respondedMovie"
+      />
       <v-container>
-        <MovieItem />
+        <MovieItem
+          v-for="movie in clickedItemArr"
+          :key="movie.Title"
+          :movie="movie"
+        />
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import { runGetRequestWithParams } from "./API/GetRequest";
+import { ref, onMounted } from "@vue/composition-api";
 import SearchBar from "./components/SearchBar.vue";
 import MovieItem from "./components/MovieItem.vue";
 
 export default {
-  data: () => ({}),
+  setup() {
+    let respondedMovie = ref([]);
+    let isListOpen = ref(false);
+    let clickedItemArr = ref([]);
+  
+    onMounted(() => {
+      console.log("a");
+    });
+
+    async function searchByInputValue(movieTitle, movieIsEmpty) {
+      if (movieIsEmpty) return;
+      const response = await runGetRequestWithParams(movieTitle);
+      respondedMovie.value = [response.data];
+      console.log(respondedMovie.value);
+      isListOpen.value = true;
+      console.log(isListOpen.value);
+    }
+
+    function getAndPassClickedValue(clickedItem) {
+      isListOpen.value = false;
+      clickedItemArr.value.push(clickedItem); // save to local storage
+
+      localStorage.setItem("movies", JSON.stringify(clickedItemArr));
+
+      console.log(clickedItemArr);
+    }
+
+    return {
+      isListOpen,
+      respondedMovie,
+      searchByInputValue,
+      getAndPassClickedValue,
+      clickedItemArr,
+    };
+  },
   components: {
     SearchBar,
     MovieItem,
