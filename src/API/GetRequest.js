13e5dd16f78be5eddec9cce131a11349f80f28f7
ipek sargin <ipek.srgn@gmail.com>
2021-04-23 02:35:41 +0300

    https changed

--- src/API/GetRequest.js
+++ src/API/GetRequest.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 export const runGetRequestWithParams = async (movieTitle) => {
     const year = "2020";
     const apiKey = "71cb4251"
-    const URL = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}&y=${year}`;
+    const URL = `https://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}&y=${year}`;
     const response = await axios.get(URL);
 
     if (response.status != 200){
